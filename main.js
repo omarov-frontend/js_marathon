871f2f153bf29c8c0155d4f578922ec934a1b31e
Shamil Omarov <s_omarov05@mail.ru>
2020-09-26 21:57:25 +0300

    HomeWork 3

--- main.js
+++ main.js
@@ -1,59 +1,57 @@
-// [HomeWork 2] - Задача #1:
-const firstRow = 'мама мыла раму';
-const secondRow = 'собака друг человека';
-const CHAR = 'а';
-const value = prompt('Введите букву:', CHAR);
-
-function getRow(firstRow, secondRow, CHAR, value) {  
-    let countFirstValue = countedChar(firstRow, CHAR);
-    let countSecondValue = countedChar(secondRow, CHAR);
-
-    if (value.length > 1) return 'Вы ввели больше одного символа!';
-    if (countFirstValue === countSecondValue) return `Здесь одинаковое кол-во букв - '${CHAR}'`;
-
-    return countFirstValue > countSecondValue ? firstRow : secondRow;
+const $btn = document.getElementById('btn-kick');
+const character = {
+    name: 'Pikachu',
+    defaultHP: 100,
+    damageHP: 100,
+    elHP: document.getElementById('health-character'),
+    elProgressbar: document.getElementById('progressbar-character'),
 }
 
-function countedChar(row, char) {
-    let counted = 0;
-  
-    for (let i = 0; i < row.length; i++) {
-        if (row.charAt(i) === char) {
-            counted++;
-        }
+const enemy = {
+    name: 'Charmander',
+    defaultHP: 100,
+    damageHP: 100,
+    elHP: document.getElementById('health-enemy'),
+    elProgressbar: document.getElementById('progressbar-enemy'),
+}
+
+$btn.addEventListener('click', function() {
+    console.log('Kick');
+    changeHP(random(20), character);
+    changeHP(random(20), enemy);
+});
+
+function init() {
+    console.log('Start Game!');
+    renderHP(character);
+    renderHP(enemy);
+}
+
+function renderHP(person) {
+    renderHPlife(person);
+    renderProgressbarHP(person);
+}
+
+function renderHPlife(person) {
+    person.elHP.innerText = person.damageHP + ' / '+ person.defaultHP;
+}
+
+function renderProgressbarHP(person) {
+    person.elProgressbar.style.width = person.damageHP + '%';
+}
+
+function changeHP(count, person) {
+    if(person.damageHP < count) {
+        person.damageHP = 0;
+        alert('Бедный ' + person.name + ' проиграл бой!');
+        $btn.disabled = true;
+    } else {
+        person.damageHP -= count;
     }
-    return counted;
+    renderHP(person);
 }
-alert( getRow(firstRow, secondRow, CHAR, value) );
 
-
-// [HomeWork 2] - Задача #2:
-function formattedPhone(phoneValue) {
-    if (!phoneValue) return 'Вы не ввели номер телефона!';
-      
-    const clearSpaces = /[^\d]/g;
-    const phonePattern = /(\d{3})(\d{3})(\d{2})(\d{2})/;
-  
-    switch ( phoneValue.charAt(0) ) {
-      case '+':
-        return phoneValue.slice(2)
-          .replace(clearSpaces, '')
-          .replace(phonePattern, '+7 ($1) $2-$3-$4');
-      case '7':
-        return phoneValue.slice(1)
-          .replace(clearSpaces, '')
-          .replace(phonePattern, '+7 ($1) $2-$3-$4');
-      case '8':
-        return phoneValue.slice(1)
-          .replace(clearSpaces, '')
-          .replace(phonePattern, '+7 ($1) $2-$3-$4');
-      case '9':
-        return phoneValue
-          .replace(clearSpaces, '')
-          .replace(phonePattern, '+7 ($1) $2-$3-$4');
-      default:
-        return 'Введен неверный формат телефона!';
-    }
+function random(num) {
+    return Math.ceil( Math.random() * num );
 }
-const phoneValue = prompt('Введите номер телефона:', '');
-alert(formattedPhone(phoneValue));
+init();
